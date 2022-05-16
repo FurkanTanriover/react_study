ac573ad223c1606789cecbe7ae351eee9ce11d3c
Furkan Tanrıöver <56587949+FurkanTanriover@users.noreply.github.com>
2022-05-16 22:27:17 +0300

    project updated
    bootstrap and some components added

---
+++ react_study/src/component/User.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+
+//class component oluşturma
+class User extends Component {
+  render() {
+    return (
+      <div>
+          <form>
+              <input type="text"></input>
+              <button>Gönder</button>
+          </form>
+      </div>
+    )
+  }
+}
+
+export default User;
