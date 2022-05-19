b3d83a4bbbf2f57e8f42b6957fe7aba5d0273921
Furkan Tanrıöver <56587949+FurkanTanriover@users.noreply.github.com>
2022-05-19 21:42:59 +0300

    project updated

--- react_study/src/component/User.js
+++ react_study/src/component/User.js
@@ -1,7 +1,7 @@
 //import React, { Component } from 'react'
 
 import React, {useState}from 'react'
-
+import UserConsumer from '../context';
  //function component ve hook kullanarak
 
  function User(props) {
@@ -10,24 +10,40 @@ import React, {useState}from 'react'
   //burada array destructuring(dizi parçalama) var useState tarafından dönen ilk değer isVisible ikinci değer ise setVisible
   const [isVisible,setVisible]=useState(false);
 
-  return (
-    <div className='col-md-8 mb-4'>
-        <div className='card' onClick={()=>setVisible(!isVisible)}>
-          <div className='card-header d-flex justify-content-between'>
-            <h4 className='d-inline'>{name}</h4>
-            <i className='far fa-trash-alt' style={{cursor:"pointer"}}> </i>
-          </div>
-          {isVisible ? 
-          <div className='card-body'>
-          <p className='card-text'>Maaş : {salary}</p>
-          <p className='card-text'>Department : {department}</p>
-    
-        </div>:null
+  const onDeleteUser=(dispatch,e)=>{
+    const id=props;
+    dispatch({type:"DELETE_USER,payload:id"});
+  }
+
+    return(
+      <UserConsumer>
+        {
+          value=>{
+            const dispatch=value;
+            return (
+              <div className='col-md-8 mb-4'>
+                  <div className='card' onClick={()=>setVisible(!isVisible)}>
+                    <div className='card-header d-flex justify-content-between'>
+                      <h4 className='d-inline'>{name}</h4>
+                      <i onClick={()=>onDeleteUser()} className='far fa-trash-alt' style={{cursor:"pointer"}}> </i>
+                    </div>
+                    {isVisible ? 
+                    <div className='card-body'>
+                    <p className='card-text'>Maaş : {salary}</p>
+                    <p className='card-text'>Department : {department}</p>
+              
+                  </div>:null
+                    }
+                 
+                  </div>
+              </div>
+            ) 
           }
-       
-        </div>
-    </div>
-  ) 
+        }
+      </UserConsumer>
+    )
+
+  
 }
 
 export default User;
--- react_study/src/context.js
+++ react_study/src/context.js
@@ -4,51 +4,66 @@ const userContext=React.createContext();
 //Provider ve Consumer alıyoruz
 
 
-export function UserProvider(props) {
+
+const reducer = (state,action)=>{
+    switch(action.type){
+        case "DELETE_USER":
+            return {
+                ...state,
+                users:state.users.filter(user=>action.payload!==user.id)
+            }
+        }
+    }
+    export function UserProvider(props) {
+
     const [users]=useState([
         {
-          id:1,
-          name:"Furkan ",
-          salary:"5000",
-          department:"Bilişim"
-        },
-        { 
-          id:2,
-          name:"Fatih",
-          salary:"5000",
-          department:"Bilişim"
-        },
-        { 
-          id:3,
-          name:"Aleyna ",
-          salary:"5000",
-          department:"Bilişim"
+            id:1,
+            name:"Furkan ",
+            salary:"5000",
+            department:"Bilişim"
         },
         {
-          id:4,
-          name:"Palavan ",
-          salary:"5000",
-          department:"Bilişim"
+            id:2,
+            name:"Fatih",
+            salary:"5000",
+            department:"Bilişim"
         },
         {
-          id:5,
-          name:"Selim ",
-          salary:"5000",
-          department:"Bilişim"
+            id:3,
+            name:"Aleyna ",
+            salary:"5000",
+            department:"Bilişim"
         },
         {
-          id:6,
-          name:"Aliş ",
-          salary:"5000",
-          department:"Bilişim"
+            id:4,
+            name:"Palavan ",
+            salary:"5000",
+            department:"Bilişim"
         },
-    ])  
-  return (
-    <userContext.Provider value={users}>
-        {props.children} 
+        {
+            id:5,
+            name:"Selim ",
+            salary:"5000",
+            department:"Bilişim"
+        },
+        {
+            id:6,
+            name:"Aliş ",
+            salary:"5000",
+            department:"Bilişim"
+        },
+    ]);
+
+    return (
+        <userContext.Provider value={users}>
+        {props.children}
     </userContext.Provider>
   )
 }
+dispatch(action =>{
+    state => reducer(state,action);
+});
 const UserConsumer=userContext.Consumer;
 
 export default UserConsumer;
