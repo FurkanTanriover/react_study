6e0cb09ae19d806aaae6388c1291b8a2f0eea17f
Furkan Tanrıöver <56587949+FurkanTanriover@users.noreply.github.com>
2022-05-27 02:44:48 +0300

    checkbox and select train

--- react_mui/react_mui_demo/src/App.tsx
+++ react_mui/react_mui_demo/src/App.tsx
@@ -2,17 +2,21 @@ import { Stack } from '@mui/material';
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
-import { MuiButton } from './component/MuiButton';
-import { MuiTextField } from './component/MuiTextField';
-import { MuiTypography } from './component/MuiTypography';
+import { MuiCheckbox } from './component/MuiCheckbox';
+// import { MuiButton } from './component/MuiButton';
+// import { MuiSelect } from './component/MuiSelect';
+// import { MuiTextField } from './component/MuiTextField';
+// import { MuiTypography } from './component/MuiTypography';
 
 function App() {
   return (
     <div className="App">
       <Stack spacing={8}>
-      {/* <MuiTypography/>   
-      <MuiButton/>    */}
-      <MuiTextField/>
+        {/* <MuiTypography/>
+        <MuiTextField/>
+        <MuiButton/>
+      <MuiSelect/> */}
+      <MuiCheckbox/>
       </Stack>
     </div>
   );
