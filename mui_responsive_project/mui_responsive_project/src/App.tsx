d10e6bcb154f7e32baec219c9527432e6e6ffcb8
Furkan Tanrıöver <56587949+FurkanTanriover@users.noreply.github.com>
2022-06-01 14:45:05 +0300

    project updated

--- mui_responsive_project/mui_responsive_project/src/App.tsx
+++ mui_responsive_project/mui_responsive_project/src/App.tsx
@@ -1,11 +1,40 @@
-import { Box } from '@mui/material';
+import { createTheme, PaletteMode, Stack, ThemeProvider } from '@mui/material';
+import { Box } from '@mui/system';
+import { useState } from 'react';
 import './App.css';
+import Add from './components/Add';
+import Feed from './components/Feed';
+import Navbar from './components/Navbar';
+import Rightbar from './components/Rightbar';
+import Sidebar from './components/Sidebar';
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const theme=createTheme ({
+    palette:{
+      mode:mode as PaletteMode
+    }
+  })
+
+  
+  
+
+  const MySideBar=({mode,setMode}:any)=>{
+    return <Sidebar mode={mode} setMode={setMode}/>;
+  }
+
   return (
-    <Box>
-      hello dsfdasf
-    </Box>
+      <ThemeProvider theme={theme}>
+        <Box bgcolor={"background.default"} color={"text.primary"}>
+          <Navbar/>
+          <Stack direction={"row"} justifyContent={"center"} spacing={1}>
+            <MySideBar setMode={setMode} mode={mode}/>
+            <Feed/>
+            <Rightbar/>
+          </Stack>
+          <Add/>
+        </Box>
+      </ThemeProvider>
   );
 }
 
